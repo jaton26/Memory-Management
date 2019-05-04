afa2b2dccc7c725517f426b42eb442c4c988be74
NuttySalmon <nuttysalmon@gmail.com>
2019-05-04 11:18:11 -0700

    Implemented function in Process class

---
+++ src/process.cpp
@@ -0,0 +1,37 @@
+#include "process.hpp"
+#include <sstream>
+
+int Process::getId(){
+	return id;
+}
+
+
+void Process::addPage(Page* newP){
+	int virAdd = newP->getVirAdd();
+	pageTable[virAdd] = newP;
+}
+
+
+Page* Process::free(int virAdd){
+	Page* temp = pageTable[virAdd];
+	pageTable.erase(virAdd);
+	return temp;
+}
+	
+std::string Process::printPageTable(){
+	std::stringstream ss;
+	ss << "PROCESS " << id << "\n";
+	for (std::map<int,Page*>::iterator it=pageTable.begin(); it!=pageTable.end(); ++it){
+		ss << "Virtual\t" << it->first << "\t";
+		Page* temp = it->second;
+		if(temp->isSwapped()){
+			ss << "SWAP\t";
+		} else{
+			ss << "PHYSICAL\t" << temp->getPhyAdd();
+		}
+		ss << "\n";
+	}
+
+	return ss.str();
+}
+
